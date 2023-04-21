7e1af7aacaaa058c25042debbe05b5194cf97777
Luiz Eduardo <gs.luizeduardo00@gmail.com>
2023-04-21 17:20:17 -0300

    feat: add delete icon and other fields

--- src/features/category/ListCategory.tsx
+++ src/features/category/ListCategory.tsx
@@ -1,23 +1,72 @@
-import { Box, Button, Typography } from '@mui/material';
+import { Box, Button, IconButton, Typography } from '@mui/material';
 import { useAppSelector } from '../../app/hooks';
 import { selectCategory } from './categorySlice';
 import { Link } from 'react-router-dom';
-import { DataGrid, GridColDef, GridRowsProp, GridValueGetterParams } from '@mui/x-data-grid';
+import DeleteIcon from '@mui/icons-material/Delete';
+import { DataGrid, GridColDef, GridRenderCellParams, GridRowsProp } from '@mui/x-data-grid';
 
 export const ListCategory = () => {
   const categories = useAppSelector(selectCategory);
 
   const columns: GridColDef[] = [
-    { field: 'id', headerName: 'ID', width: 300 },
-    { field: 'name', headerName: 'Name', width: 150 },
-    { field: 'description', headerName: 'Description', width: 400 },
+    // { field: 'id', headerName: 'ID' },
+    {
+      field: 'name',
+      headerName: 'Name',
+      flex: 1,
+    },
+    {
+      field: 'description',
+      headerName: 'Description',
+      flex: 1,
+    },
+    {
+      field: 'is_active',
+      headerName: 'Active',
+      flex: 1,
+      type: 'boolean',
+      renderCell: renderIsActiveCell
+    },
+    {
+      field: 'created_at',
+      headerName: 'Created',
+      flex: 1,
+    },
+    {
+      field: 'actions',
+      headerName: 'Actions',
+      flex: 1,
+      renderCell: renderActionsCell
+    },
   ];
 
   const rows: GridRowsProp = categories.map(category => ({
     id: category.id,
     name: category.name,
     description: category.description,
+    is_active: category.is_active,
+    created_at: new Date(category.created_at).toLocaleDateString('pt-BR'),
   }));
+
+  function renderActionsCell(params: GridRenderCellParams) {
+    return (
+      <IconButton
+        color='secondary'
+        onClick={() => console.log("deletar")}
+        aria-label='delete'
+      >
+        <DeleteIcon />
+      </IconButton>
+    );
+  }
+
+  function renderIsActiveCell(row: GridRenderCellParams) {
+    return (
+      <Typography color={row.value ? "primary" : "secondary"}>
+        {row.value ? 'Active' : 'Inactive'}
+      </Typography>
+    );
+  }
 
   return (
     <Box maxWidth='lg' sx={{ mt: 4, mb: 4 }}>
